9247e4a2219424567a15f6c766dca104040803b3
Robson Serafim <99512194+robsoncserafim@users.noreply.github.com>
2023-06-20 20:00:24 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -27,10 +27,9 @@ image = Image.open ('images/cardio_image_EDA.jpg')
 # Insert image
 st.image(image, width=800)
 
-st.markdown (
-"""
+st.markdown ("""
 
-I strongly believe that the primary objective of data science is to make a meaningful contribution to society by providing valuable insights. 
+I strongly believe that the primary objective of data science is to make a meaningful contribution to society by providing valuable insights.
 
 The healthcare industry is one area where the application of data science holds immense significance and offers numerous benefits to society.
 
@@ -38,17 +37,17 @@ The web application provides an easy-to-navigate interface where you can explore
 ### So, how to navigate in this app?
 
 **- Exploratory Data Analysis:**
-    -The Exploratory Data Analysis section presents statitical insights and visualizations using popular libraries such as Seaborn, Plotly, and Matplotlib. These visualizations help us to understanding the data and gaining insights from it.
+    - The Exploratory Data Analysis section presents statistical insights and visualizations using popular libraries such as Seaborn, Plotly, and Matplotlib. These visualizations help us understand the data and gain insights from it.
 
 **- Machine Learning:**
-    -Classification Machine Learning Model that utilizes Gradient Boosting to predict the presence or absence of a Cardiovascular disease. This model leverages the power of data to make accurate predictions and assist in healthcare decision-making.
+    - Classification Machine Learning Model that utilizes Gradient Boosting to predict the presence or absence of a Cardiovascular disease. This model leverages the power of data to make accurate predictions and assist in healthcare decision-making.
 
 
 #### Feel free to explore and interact with the application! 
 #### It provides an opportunity to gain insights and experience the predictive capabilities of the deployed Machine Learning Model.
-
      
 """)
+
 
 
 
